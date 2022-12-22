9550205f9886d5ee85e5f1735b584f6b010e24b0
student-ice <china_uos@163.com>
2022-12-22 19:34:10 +0800

    fix:九九乘法表.c

--- 九九乘法表.c
+++ 九九乘法表.c
@@ -4,7 +4,7 @@ int main(int argc, char const *argv[])
     int a,b;
     for(a=1;a<=9;a++){
         for(b=1;b<=a;b++){
-            printf("%d * %d = %-3d",b,a,a*b);
+            printf("%d*%d=%-3d",b,a,a*b);
         }
         putchar('\n');
     }
